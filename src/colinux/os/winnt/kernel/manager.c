b4787deb5ffd5fb72798cc8cda7084e26c298969
Dan Aloni <321273+da-x@users.noreply.github.com>
2004-07-10 16:56:58 +0000

    snapshot-20040710

---
+++ src/colinux/os/winnt/kernel/manager.c
@@ -0,0 +1,106 @@
+/*
+ * This source code is a part of coLinux source package.
+ *
+ * Dan Aloni <da-x@colinux.org>, 2003 (c)
+ *
+ * The code is licensed under the GPL. See the COPYING file at
+ * the root directory.
+ *
+ */
+
+#include "manager.h"
+
+#include <colinux/os/alloc.h>
+
+co_rc_t co_os_get_pfn_ptr(struct co_manager *manager, co_pfn_t pfn, co_os_mdl_ptr_t **mdl_out)
+{
+	co_os_mdl_ptr_t *current = &manager->osdep->map_root;
+	int level = MDL_MAPPING_LEVELS;
+
+	while (level > 0) {
+		unsigned long index;
+
+		if (current->more_mdls == NULL) {
+			current->more_mdls = co_os_malloc(sizeof(co_os_mdl_ptr_t)*MDL_MAPPING_ENTRIES);
+			if (current->more_mdls == NULL)
+				return CO_RC(ERROR);
+			manager->osdep->blocks_allocated++;
+
+			memset(current->more_mdls, 0, sizeof(co_os_mdl_ptr_t)*MDL_MAPPING_ENTRIES);
+		}
+
+		index = ((pfn >> ((level-1)*MDL_MAPPING_ENTRIES_SCALE)) & (MDL_MAPPING_ENTRIES - 1));
+		current = &current->more_mdls[index];
+		level -= 1;
+	}
+	
+	*mdl_out = current;
+
+	return CO_RC(OK);
+}
+
+void co_os_free_pfn_tracker(co_os_mdl_ptr_t *current, int level, unsigned long *pages, unsigned long *lists)
+{
+	int i;
+
+	if (level == 0) {
+		switch (current->type) {
+		case CO_OS_MDL_PTR_TYPE_MDL:
+			if (current->mdl != NULL) {
+				(*pages)++;
+				MmFreePagesFromMdl(current->mdl);
+				IoFreeMdl(current->mdl);
+			}
+			break;
+		case CO_OS_MDL_PTR_TYPE_PAGE:
+			if (current->page != NULL) {
+				(*pages)++;
+				co_os_free_pages(current->page, 1);
+			}
+			break;
+		default:
+			break;
+		}
+		return;
+	}
+
+	if (current->more_mdls == NULL)
+		return;
+
+	for (i=0; i < MDL_MAPPING_ENTRIES; i++)
+		co_os_free_pfn_tracker(&current->more_mdls[i], level-1, pages, lists);
+
+	co_os_free(current->more_mdls);	
+	(*lists)++;
+}
+
+co_rc_t co_os_manager_init(co_manager_t *manager, co_osdep_manager_t *osdep)
+{
+	co_rc_t rc = CO_RC(OK);
+
+	*osdep = (typeof(*osdep))(co_os_malloc(sizeof(**osdep)));
+	if (*osdep == NULL)
+		return CO_RC(ERROR);
+
+	co_global_manager = manager;
+
+	memset(*osdep, 0, sizeof(**osdep));
+
+	MmResetDriverPaging(&co_global_manager);
+
+	return rc;
+}
+
+void co_os_manager_free(co_osdep_manager_t osdep)
+{
+	unsigned long pages = 0;
+	unsigned long lists = 0;
+
+	co_os_free_pfn_tracker(&osdep->map_root, MDL_MAPPING_LEVELS, &pages, &lists);
+
+	co_debug("peak allocation: %d mdls, %d aux\n", osdep->mdls_peak_allocation, osdep->auxiliary_peak_allocation);
+	co_debug("freed: %d pages, %d lists\n", pages, lists);
+
+	co_os_free(osdep);
+}
+
---
+++ src/colinux/os/winnt/user/console/head.cpp
@@ -0,0 +1,125 @@
+/*
+ * This source code is a part of coLinux source package.
+ *
+ * Dan Aloni <da-x@gmx.net>, 2003-2004 (c)
+ *
+ * The code is licensed under the GPL. See the COPYING file at
+ * the root directory.
+ *
+ */ 
+
+#include <windows.h>
+
+extern "C" {
+	#include <colinux/common/debug.h>
+}
+
+#include <colinux/user/console/main.h>
+
+COLINUX_DEFINE_MODULE("colinux-console-fltk");
+
+static char scancode_state[0x100];
+static char scancode_state_e0[0x100];
+
+static HHOOK current_hook;
+static LRESULT CALLBACK keyboard_hook(
+    int nCode,
+    WPARAM wParam,
+    LPARAM lParam
+)
+{
+	int AltDown = (lParam & (1 << 29)) ? 1 : 0;
+	co_scan_code_t sc;
+	sc.code = (lParam >> 16) & 0xff;
+	sc.down = (lParam & (1 << 31)) ? 0 : 1;
+
+	if ((wParam == VK_MENU) && (HIWORD(lParam) & KF_EXTENDED)) {
+		// AltGr key comes in as Control+Right Alt
+		scancode_state_e0[0x38] = sc.down;
+
+		if (sc.down) {
+			// Release Control key
+			sc.code = 0x9d;
+			co_user_console_handle_scancode(sc);
+
+			// Send extended scan code
+			sc.code = 0xe0;
+			co_user_console_handle_scancode(sc);
+			sc.code = 0x38;
+			co_user_console_handle_scancode(sc);
+		} else {
+			// Send extended scan code
+			sc.code = 0xe0;
+			co_user_console_handle_scancode(sc);
+			sc.code = 0x38;
+			co_user_console_handle_scancode(sc);
+		}
+	} else {
+		scancode_state[sc.code] = sc.down;
+		
+		co_user_console_handle_scancode(sc);
+	}
+
+	if ((AltDown == 1) &&
+            (wParam == 0x73)) {
+		// Swallow the ALT-F4 keystroke.
+		return 1;
+	}
+
+	return CallNextHookEx(current_hook, nCode, wParam, lParam);
+}
+
+void co_user_console_keyboard_focus_change(unsigned long keyboard_focus)
+{
+	if (keyboard_focus == 0) {
+		int i;
+
+		/*
+		 * Lost keyboard focus. Release all pressed keys.
+		 */ 
+
+		for (i=0; i < 0x100; i++) {
+			if (scancode_state[i]) {
+				co_scan_code_t sc;
+				sc.code = i;
+				sc.down = 0;
+				scancode_state[sc.code] = 0;
+
+				co_user_console_handle_scancode(sc);
+			}
+
+			if (scancode_state_e0[i]) {
+				co_scan_code_t sc;
+				
+				sc.code = 0xe0;
+				sc.down = 0;
+				
+				co_user_console_handle_scancode(sc);
+				
+				sc.code = i;
+				sc.down = 0;
+				scancode_state_e0[sc.code] = 0;
+
+				co_user_console_handle_scancode(sc);
+			}
+		}
+	}
+}
+
+int main(int argc, char **argv) 
+{
+	int i;
+	
+	// Initialize scancode state arrays
+	for (i=0; i < 0x100; i++) {
+		scancode_state[i] = 0;
+		scancode_state_e0[i] = 0;
+	}
+	
+	current_hook = SetWindowsHookEx(WH_KEYBOARD,
+					keyboard_hook,
+					NULL,
+					GetCurrentThreadId());
+
+	return co_user_console_main(argc, argv);
+}
